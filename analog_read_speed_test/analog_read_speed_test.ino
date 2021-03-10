eb26b91dd768177b95801eb77fc8dfa400f75ca9
NahidNazmun <72479431+NahidNazmun@users.noreply.github.com>
2021-03-10 20:49:17 +0900

    Add files via upload

---
+++ analog_read_speed_test/analog_read_speed_test.ino
@@ -0,0 +1,23 @@
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  unsigned long t = micros();
+  for (int i = 0; i < 1000; i++)
+  {
+    int sensorValue1 = analogRead(A0);
+    int sensorValue2 = analogRead(A1);
+    int sensorValue3 = analogRead(A2);
+    int sensorValue4 = analogRead(A3);
+    int sensorValue5 = analogRead(A4);
+    int sensorValue6 = analogRead(A5);
+    int sensorValue7 = analogRead(A6);
+    int sensorValue8 = analogRead(A7);
+  }
+  unsigned long d = micros();
+  Serial.println(d-t);
+  delay(500);
+}
